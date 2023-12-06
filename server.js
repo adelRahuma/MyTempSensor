36d2e75305aad67e052c0ff6a94f533ef91d6bd5
AdelRahuma <adel_bugbug@yahoo.com>
2023-12-06 14:07:38 +0000

    repo changed

--- server.js
+++ server.js
@@ -1,53 +1,57 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
-const mysql = require("mysql2");
+const mysql = require("mysql2/promise"); // Using promise-based MySQL
+const cors = require("cors");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000; // Use the specified port in the environment or default to 3000
 
 const pool = mysql.createPool({
   host: process.env.HOST,
   user: process.env.MYUSER,
   password: process.env.PASSWD,
   database: process.env.DATABASE,
+  waitForConnections: true, // Wait for available connection if the limit is reached
+  connectionLimit: 10, // Maximum number of connections in the pool
+  queueLimit: 0, // Unlimited queued connection requests
 });
-
+// Enable CORS for all routes
+app.use(cors());
 app.use(express.json());
-app.post("/arduino-data", (req, res) => {
-  const data = req.body.data;
-  //console.log(data,"data<<<<");
-  pool.getConnection((err, connection) => {
-    if (err) {
-      console.error("Error connecting to database:", err);
-      res.status(500).send("Internal Server Error");
-      return;
-    }
-    connection.changeUser({ database: process.env.DATABAS }, (err) => {
-      if (err) {
-        console.error("Error selecting database:", err);
-        connection.release();
-        res.status(500).send("Internal Server Error");
-        return;
-      }
 
-      const query = "INSERT INTO TempSensor(TEMP) values(?)";
-      connection.query(query, [data], (err, results) => {
-        connection.release();
-
-        if (err) {
-          console.error("Error executing query:", err);
-          res.status(500).send("Internal Server Error");
-          return;
-        }
-
-        //console.log(results);
-        res.status(200).send("Data inserted successfully");
-      });
-    });
-  });
+app.post("/arduino-data", async (req, res) => {
+  try {
+    const data = req.body.data;
+    const connection = await pool.getConnection();
+    await connection.changeUser({ database: process.env.DATABASE });
+    const query = "INSERT INTO TempSensor(TEMP) VALUES (?)";
+    const [results] = await connection.execute(query, [data]);
+    connection.release();
+    res.status(201).json({ result: results });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
+app.get("/data", async (req, res) => {
+  try {
+    const connection = await pool.getConnection();
+    await connection.changeUser({ database: process.env.DATABASE });
+    const query = "SELECT * FROM TempSensor";
+    const [results] = await connection.execute(query);
+    connection.release();
+    res.send({ data: results }); // Wrap the results in an object
+  } catch (error) {
+    console.error("Error:", error);
+    res.send({ error: "Internal Server Error", details: error.message });
+  }
+});
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
+
 module.exports = app;
